d73c2f1c7267677bd6e483afad7dd13a032f950a
mayks1 <mayks1@gmail.com>
2019-09-07 20:44:46 +0300

    Added  BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,39 @@
-const gulp    = require('gulp');
-const sass          = require('gulp-sass');
-const browserSync   = require('browser-sync').create();
+const {src, dest, watch}        = require('gulp');
+const autoprefixer              = require('autoprefixer')
+const cssnano                   = require('cssnano')
+const postcss                   = require('gulp-postcss')
+const sourcemaps                = require('gulp-sourcemaps')
+const sass                      = require('gulp-sass')
+const browserSync               = require('browser-sync').create()
 
+// FILE PATH
+const sassPath      = './app/sass/**/*.sass'
+const jsPath        ='./app/js/**/*.js'
+const cssPath       = './dist/css'
 
+// SASS TASK
 //COMPILE SASS TO CSS
-function style () {
+function sassTask () {
 
     //Were is the SASS FILE
-    return gulp.src('./app/sass/**/*.sass')
+    return src(sassPath)
+    // SOURCE MAP
+    .pipe(sourcemaps.init())
     // Compile the SASS FILE TO CSS
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
+    // AUTOPREFIXER AND CSS NANO
+    .pipe(postcss([ autoprefixer, cssnano ]))
+    // WRITE SOURCE MAP
+    .pipe(sourcemaps.write('.'))
     // DESTINATION file css
-    .pipe( gulp.dest('./dist/css'))
+    .pipe(dest(cssPath))
 
     // STREAM CHANGES TO ALL BROWSERS
     .pipe(browserSync.stream())
-
 }
 
-function watch() {
+
+function compile() {
     //Initialize BrowserSync --- Base Directory './'
     browserSync.init({
         server: {
@@ -27,14 +42,14 @@ function watch() {
     });
 
     //Watch the SASS Directory and run Style Function
-    gulp.watch('./app/sass/**/*.sass', style);
+    watch('./app/sass/**/*.sass', sassTask);
 
     //Watch the HTML Files and Reload BrowserSync
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    watch('./*.html').on('change', browserSync.reload);
 
      //Watch the JS Files and Reload BrowserSync
     //  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
-exports.style = style;
-exports.watch = watch;
+
+exports.default = compile;
